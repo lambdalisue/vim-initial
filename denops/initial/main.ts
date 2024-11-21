4bd12eec6929106ca342f9171cd3bc6c729e1dc2
Alisue <lambdalisue@gmail.com>
2024-11-22 01:55:28 +0900

    Add polyfill for DisposableStack and AsyncDisposableStack

--- denops/initial/main.ts
+++ denops/initial/main.ts
@@ -1,3 +1,5 @@
+import "./polyfill.ts";
+
 import type { Denops, Entrypoint } from "jsr:@denops/std@^7.3.2";
 import { collect } from "jsr:@denops/std@^7.3.2/batch";
 import * as fn from "jsr:@denops/std@^7.3.2/function";
